30561e1652652da824c44551c9a10bb4a1136459
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-03 01:12:38 +0100

    refractor: add mongoDb connection to new database

--- src/services/db.js
+++ src/services/db.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", false);
 
 
 export const connectToDb = async () => {
-
+    const uri = `mongodb+srv://${process.env.USER}:${process.env.USER}@nasa-app.hficxsv.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
     mongoose
         .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log("DB CONNECTED!"))
