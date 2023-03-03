0137dfbc8c3b7c99203e7e84d78115ac6f2fe133
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-03 11:34:56 +0100

    feat: user crud added and controlled errors

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,76 +3,90 @@ const Rover = require("../models/rover.js");
 // import Apod from "../models/apod.js";
 
 const getUserList = async () => {
-
-    const user = await User.find();
-    return user
-
+    try {
+        const user = await User.find();
+        return user;
+    } catch (error) {
+        console.log('Error at bring all users ' + error.message);
+    }
 }
 
 const getUserId = async (id) => {
-
-    const user = await User.findById(id)
-    return user
+    try {
+        if(!id) throw new Error('Can not find use by id, id is null');
+        const user = await User.findById(id);
+        return user;
+    } catch (error) {
+        console.log('Error at get user by id ' + error.message);
+    }
 }
 
 const getUserByEmail = async (email) => {
     try {
-        const user = await User.findOne({ email })
-        return user 
+        const user = await User.findOne({ email });
+        return user;
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 }
 
-const createUser = async ({ name, email, password }) => {
-
-    const user = new User({ name, email, password });
-    return user.save();
-
+const createUser = async (data = { name, email, password }) => {
+    try {
+        if(!data) throw new Error('Can not create user, any data null...');
+        const user = new User({ name, email, password });
+        return user.save();
+    } catch (error) {
+        console.log('Error at create user ' + error.message);
+    }
 }
 
 const updateUser = async (id, data) => {
-
-    const user = await getUserId(id);
-    await user.updateOne(data)
-    return data
+    try {
+        if(!id || !data) throw new Error('Can not update user id or data null...')
+        const user = await getUserId(id);
+        await user.updateOne(data);
+    return data;
+    } catch (error) {
+        console.log('Error at update user ' + error.message);
+    }
 }
 
 const deleteUser = async ({ id }) => {
-    await User.findOneAndRemove(id)
-    return true
+    try {
+        if(!id) throw new Error('Can not delete use, id given null...')
+        await User.findOneAndRemove(id)
+        return true;
+    } catch (error) {
+        console.log('Error at update user ' + error.message);
+    }
 }
 
 const updateUserFavList = async ({ id, idNasa }) => {
 
     try {
-
+        if(!id || !idNasa) throw new Error('id or idNasa null given, can not update favorites');
         const user = await getUserId(id);
-        const currentFavList = user.favList
-        let newFavsList = currentFavList
-        const existed = currentFavList.includes(idNasa)
-        const roverDB = await Rover.findById(idNasa)
+        const currentFavList = user.favList;
+        let newFavsList = currentFavList;
+        const existed = currentFavList.includes(idNasa);
+        const roverDB = await Rover.findById(idNasa);
 
         if (existed) {
-            newFavsList = currentFavList.filter(item => item !== idNasa)
-            console.log("Este documento ha sido eliminado")
+            newFavsList = currentFavList.filter(item => item !== idNasa);
+            console.log("Eliminado de favoritos");
         } else if (roverDB) {
-            newFavsList.push(idNasa)
-            console.log("Este documento ha sido insertado")
+            newFavsList.push(idNasa);
+            console.log("Añadido a favoritos");
         }
 
-        await User.findByIdAndUpdate(id, { favList: newFavsList })
-
-        let userUpdate = await getUserId(id)
-        userUpdate = JSON.parse(JSON.stringify(userUpdate))
-        // console.log(userUpdate)
-
+        await User.findByIdAndUpdate(id, { favList: newFavsList });
+        let userUpdate = await getUserId(id);
+        userUpdate = JSON.parse(JSON.stringify(userUpdate));
         const { password, salt, ...userUpdate_ } = userUpdate;
 
-        return userUpdate_
-
+        return userUpdate_;
     } catch (error) {
-        // console.log(error.message)
+        console.log('Error at add to favorites ' + error.message);
     }
 
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -13,47 +13,49 @@ const {
 
 routerUser.get('/', async (req, res) => {
     try {
-        const users = await getUserList()
-        res.status(200).json(users)
+        const users = await getUserList();
+        !users &&  res.status(403).json('ERROR 403, users  not found');
+        users && res.status(200).json(users);
     } catch (error) {
-        res.response.status(500)
+        res.response.status(500).json('Error 500, ' + error.message);
     }
-})
+});
 
 
 routerUser.get('/:id', async (req, res) => {
     try {
-        const { id } = req.params
-        const task = await getUserId(id)
-        res.status(200).json({ _id: task._id, email: task.email, favList: task.favList })
+        !req.params.id && res.status(403).json('ERROR 403, params id not found');
+        const { id } = req.params;
+        const user = await getUserId(id);
+        !user && res.status(403).json('ERROR 403, user  not found');
+        res.status(200).json({ _id: user._id, email: user.email, favList: user.favList });
     } catch (error) {
-        res.status(500).json(error.message)
+        res.response.status(500).json('Error 500, ' + error.message);
     }
 });
-routerUser.post('/favBookList/:idNasa', async (req, res) => {
-
+routerUser.post('/favRoverList/:idRover', async (req, res) => {
     try {
-        const { idNasa } = req.params
-        const user = await updateUserFavList({ id: req.user.id, idNasa })
-        if (user === undefined) {
-            return res.status(200).json("Data no exist in database")
+        !req.params.idRover && res.status(403).json('ERROR 403, params idRover not found');
+        const { idRover } = req.params;
+        const user = await updateUserFavList({ id: req.user.id, idRover });
+        if (!user) {
+            return res.status(200).json("Data no exist in database");
         }
-        res.status(200).json("Data updated successfully")
-
+        res.status(200).json("Data updated successfully");
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 
 });
 
 
-routerUser.get('/favBookList/:idNasa', async (req, res) => {
+routerUser.get('/favBookList/:idUser', async (req, res) => {
     try {
-        const { idNasa } = req.params
-        const user = await getUserId(idNasa)
-        const favorites = user.favList
-        res.status(200).json(favorites)
-
+        !req.params.idUser && res.status(403).json('ERROR 403, params idUser not found');
+        const { idUser } = req.params;
+        const user = await getUserId(idUser);
+        const favorites = user.favList;
+        res.status(200).json(favorites);
     } catch (error) {
         res.status(500).json(error.message)
     }
