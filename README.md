f65cb84bb7241694519a692ef82ed00d14406905
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-06 00:39:28 +0100

    fix:some endpoints problems

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # EXAMEN_SegundoTrimestre_DWEC
+
 Examen segundo trimestre Desarrollo Web Entorno Cliente
 
 ## LEVANTAR SERVIDOR Y BBDD
 
-- npm run dev
+-   npm run dev
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public/"));
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views/");
-app.use("/", require("./router/apods"));
+app.use(express.static(__dirname + '/public/'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views/');
+app.use('/', require('./router/apods'));
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@nasa-app.hficxsv.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 mongoose
     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("DB CONNECTED"))
+    .then(() => console.log('DB CONNECTED'))
     .catch((e) => console.log(e.message));
 
 app.use((req, res) => {
-    res.status(404).render("404", {
-        title: "Error 404",
-        des: "Page not Found",
+    res.status(404).render('404', {
+        title: 'Error 404',
+        des: 'Page not Found',
     });
-})
-.listen(port);
-
-console.log("APP RUNNING ON PORT 8000");
+}).listen(port);
+console.log('APP RUNNING ON PORT 8000');
